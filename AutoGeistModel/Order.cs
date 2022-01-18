63f4f66ed6a96689b6aeaf77bc4fb503a257213d
I2.Les. Sorina <les.sorina@yahoo.com>
2022-01-18 14:04:57 +0200

    Add project files.

---
+++ AutoGeistModel/Order.cs
@@ -0,0 +1,23 @@
+namespace AutoGeistModel
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Order")]
+    public partial class Order
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int OrderID { get; set; }
+
+        public int? CustID { get; set; }
+
+        public int? CarID { get; set; }
+
+        public virtual Car Car { get; set; }
+
+        public virtual Customer Customer { get; set; }
+    }
+}
